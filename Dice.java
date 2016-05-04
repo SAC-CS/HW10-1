f73dae51e6ba7acc776bd626f5f718488aaa9d02
SAC-CS112-A-Adrian <pw2422@gmail.com>
2016-05-04 03:57:29 -0700

    Added files via upload

---
+++ Dice.java
@@ -0,0 +1,30 @@
+
+public class Dice {
+	
+	private int maximum;
+	private int numberOfThrows;
+	
+	public Dice ()
+	{
+		maximum = 6;
+		numberOfThrows = 0;
+	}
+	
+	public int rollDice (int number)
+	{
+		numberOfThrows++;
+		maximum = maximum * number;
+		return ( 1 + (int) (Math.random() * maximum) );
+	}
+	
+	public void gamesLossWon(int loss, int won)
+	{
+		System.out.printf("Games loss: %d\nGames won: %d\n", loss, won);
+	}
+	
+	public void numberThrows()
+	{
+		System.out.printf("Number of throws: %d\n", numberOfThrows);
+	}
+	
+}
